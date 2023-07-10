f77d781f24193deeb25b98748d8889901fdce51d
roland31x <roland31x@gmail.com>
2023-07-10 21:40:10 +0300

    day 22 working both parts

--- D22/D22/Program.cs
+++ D22/D22/Program.cs
@@ -19,87 +19,14 @@ namespace D22
                 }
             }
 
-            Node nstart = new Node(myMap.map[0][start], 0, start);
-
             Player player1 = new Player(0, start, 0, myMap);
-            NodePlayer player2 = new NodePlayer(nstart, myMap.Path);
+            Player player2 = new Player(0, start, 0, myMap);
 
             Console.WriteLine("Part1 solution");
             Console.WriteLine(player1.Part1());
             Console.WriteLine("Part2 solution");
             Console.WriteLine(player2.Part2());
 
-        }
-    }
-    public class NodePlayer
-    {
-        Node current;
-        List<Pathing> Path;
-        int dir = 0;
-        public NodePlayer(Node node, List<Pathing> path)
-        {
-            current = node;
-            Path = path;
-        }
-        void ChangeDir(string direction)
-        {
-            if (direction == "R")
-            {
-                if (dir + 1 > 3)
-                    dir = 0;
-                else
-                    dir++;
-            }
-            else
-            {
-                if (dir - 1 < 0)
-                    dir = 3;
-                else
-                    dir--;
-            }
-        }
-        public int Part2()
-        {
-            foreach(Pathing p in Path)
-            {
-                if(p.turn == null)
-                {
-                    int times = (int)p.times;
-                    int x = 0;
-                    while(x < times && current.neighbors[dir].Val == 0)
-                    {
-                        current = current.neighbors[dir];
-                        x++;
-                    }
-                }
-                else
-                {
-                    ChangeDir(p.turn);
-                }
-            }
-
-
-
-            int Score = current.mapI * 1000 + current.mapJ * 4 + dir;
-            return Score;
-        }
-    }
-    public class NodeMap
-    {
-        List<Node> nodes = new List<Node>();
-
-    }
-    public class Node
-    {
-        public Node[] neighbors = new Node[4]; // 0 - right, 1 - down, 2 - left, 3 - up
-        public int Val { get; }
-        public int mapI { get; }
-        public int mapJ { get; }
-        public Node(int value, int mapI, int mapJ)
-        {
-            Val = value;
-            this.mapJ = mapJ;
-            this.mapI = mapI;
         }
     }
     public class Player
@@ -134,13 +61,35 @@ namespace D22
         }
         public int Part1()
         {
-            foreach(Pathing p in Map.Path)
+            foreach (Pathing p in Map.Path)
             {
-                if(p.turn == null)
+                if (p.turn == null)
                 {
                     int times = (int)p.times;
                     int x = 0;
-                    while(x < times && MovePart1())
+                    while (x < times && MovePart1())
+                    {
+                        x++;
+                    }
+                }
+                else
+                {
+                    ChangeDir(p.turn);
+                }
+
+            }
+            int score = 1000 * (i + 1) + 4 * (j + 1) + dir;
+            return score;
+        }
+        public int Part2()
+        {
+            foreach (Pathing p in Map.Path)
+            {
+                if (p.turn == null)
+                {
+                    int times = (int)p.times;
+                    int x = 0;
+                    while (x < times && MovePart2())
                     {
                         x++;
                     }
@@ -152,6 +101,155 @@ namespace D22
             }
             int score = 1000 * (i + 1) + 4 * (j + 1) + dir;
             return score;
+        }
+        
+        bool MovePart2()
+        {
+            int starti = i;
+            int startj = j;
+            int startdir = dir;
+            switch (dir)
+            {
+                case 0:
+                    j++;
+                    break;
+                case 1:
+                    i++;
+                    break;
+                case 2:
+                    j--;
+                    break;
+                case 3:
+                    i--;
+                    break;
+
+            }
+
+            CheckTP();
+
+            if (Map.map[i][j] == 1)
+            {
+                i = starti;
+                j = startj;
+                dir = startdir;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        void CheckTP()
+        {
+            // got sides like:  12
+            //                  3
+            //                 54
+            //                 6
+
+
+            // dirs: 0 = right, 1 = down, 2 = left, 3 = up
+            if (i < 0)
+            {
+                if(j >= 50 && j < 100) // SIDE 1 TO SIDE 6 // C
+                {
+                    i = 150 + j - 50;
+                    j = 0;
+                    dir = 0;
+                }
+                else if(j >= 100 && j < 150) // SIDE 2 TO SIDE 6 // C
+                {
+                    i = 200 - 1;
+                    j = j - 100;
+                    dir = 3;
+                }
+            }
+            else if (i >= 200) // SIDE 6 TO SIDE 2 // C
+            {
+                i = 0;
+                j = 100 + j;
+                dir = 1;
+            }
+            else if(j < 0)
+            {
+                if(i >= 100 && i < 150) // SIDE 5 TO SIDE 1 // C
+                {
+                    i = 50 - 1 - (i - 100);
+                    j = 50;
+                    dir = 0;
+                }
+                else if(i >= 150 && i < 200) // SIDE 6 TO SIDE 1 // C
+                {
+                    j = 50 + i - 150;
+                    i = 0;
+                    dir = 1;
+                }
+            }
+            else if(j >= 150) // SIDE 2 TO 4 // C
+            {
+                i = 100 + (49 - i);
+                j = 100 - 1;
+                dir = 2;
+            }
+            else if (Map.map[i][j] == 9)
+            {
+                if (i >= 0 && i < 50) // SIDE 1 TO SIDE 5 // C
+                {
+                    i = 150 - 1 - i;
+                    j = 0;
+                    dir = 0;
+                }
+                else if (i >= 50 && i < 100) // SIDE 3 range
+                {
+                    if(j >= 100 && j < 150 && dir == 0) // SIDE 3 TO SIDE 2 
+                    {
+                        j = 100 + i - 50;
+                        i = 50 - 1;
+                        dir = 3;
+
+                    }
+                    else if(j >= 100 && j < 150 && dir == 1) // SIDE 2 TO SIDE 3
+                    {
+                        i = 50 + j - 100;
+                        j = 100 - 1;
+                        dir = 2;
+
+                    }
+                    else if (j >= 0 && j < 50 && dir == 2) // SIDE 3 TO SIDE 5
+                    {
+                        j = i - 50;
+                        i = 100;
+                        dir = 1;
+                    }
+                    else if (j >= 0 && j < 50 && dir == 3) // SIDE 5 TO SIDE 3
+                    {
+                        i = 50 + j;
+                        j = 50;
+                        dir = 0;
+                    }
+                }
+                else if(i >= 100 && i < 150)
+                {
+                                                // SIDE 4 TO SIDE 2
+                    i = 50 - 1 - (i - 100);
+                    j = 150 - 1;
+                    dir = 2;
+                }
+                else if (i >= 150 && i < 200)
+                {
+                    if (j >= 50 && j < 100 && dir == 0) // SIDE 6 TO SIDE 4
+                    {
+                        j = 50 + i - 150;
+                        i = 150 - 1;
+                        dir = 3;
+                    }
+                    else if (j >= 50 && j < 100 && dir == 1) // SIDE 4 TO SIDE 6
+                    {
+                        i = 150 + j - 50;
+                        j = 50 - 1;
+                        dir = 2;
+                    }
+                }
+            }
         }
         bool MovePart1()
         {
@@ -317,6 +415,22 @@ namespace D22
             {
                 for(int j = 0; j < map[i].Count; j++)
                 {
+                    Console.Write(map[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Show(int pi, int pj)
+        {
+            for (int i = 0; i < map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Count; j++)
+                {
+                    if (i == pi && j == pj)
+                    {
+                        Console.Write("E"); 
+                        continue;
+                    }
                     Console.Write(map[i][j]);
                 }
                 Console.WriteLine();
